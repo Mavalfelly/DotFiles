d0b9818aa509f8608536c79b09e2b77ce881cd77
Felly <matthewfeliciano16@mail.com>
2025-08-10 00:13:50 -0400

    Enhance installation script to include Python build dependencies and improve pyenv setup in shell configuration

--- install.sh
+++ install.sh
@@ -36,6 +36,13 @@ install_dependencies() {
     sudo apt update
     sudo apt install -y zsh git curl wget fd-find ripgrep fzf htop tree
     sudo apt install -y build-essential
+    
+    # Install Python build dependencies
+    echo "Installing Python build dependencies..."
+    sudo apt install -y make build-essential libssl-dev zlib1g-dev \
+        libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+        libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev \
+        libffi-dev liblzma-dev python3-dev
 }
 
 # Install Neovim
@@ -69,11 +76,28 @@ install_node() {
 install_python() {
     echo "Installing Python..."
     curl https://pyenv.run | bash
-    export PYENV_ROOT="$HOME/.pyenv"
-    export PATH="$PYENV_ROOT/bin:$PATH"
-    eval "$(pyenv init -)"
     
-    # Install latest Python
+    # Set up pyenv in the shell
+    export PYENV_ROOT="$HOME/.pyenv"
+    [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init - bash)"
+    eval "$(pyenv virtualenv-init -)"
+    
+    # Add pyenv to shell rc files
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+    echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+    echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+    echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+    
+    # Also add to profile for login shells
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
+    echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
+    echo 'eval "$(pyenv init -)"' >> ~/.profile
+    
+    # Reload shell environment
+    source ~/.bashrc
+    
+    # Install latest stable Python version
     pyenv install $(pyenv install -l | grep -v '[a-zA-Z]' | tail -1)
     pyenv global $(pyenv install -l | grep -v '[a-zA-Z]' | tail -1)
     
