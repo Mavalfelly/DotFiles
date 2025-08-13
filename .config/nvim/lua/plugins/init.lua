40c2fc44d406e5c0e5f87b8f1f037fcf3bedb2ed
Felly <matthewfeliciano16@gmail.com>
2025-08-12 20:13:17 -0400

    dddddd

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -11,6 +11,7 @@ return {
     dependencies = {
       "williamboman/mason.nvim",
       "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/nvim-cmp",
       "hrsh7th/cmp-nvim-lsp",
     },
     config = function()
